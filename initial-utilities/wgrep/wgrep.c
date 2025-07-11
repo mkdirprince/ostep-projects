f5a36590c7d16d586fd96509cbdccb35b4a9e30d
mkdirprince <mkdirprince@github.com>
2025-07-11 21:03:06 +0000

    add impl for when no file is provided but searchterm is given

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[argc + 1]) {
 
  if (argc < 2) {
-  printf("wgrep: searchitem [file...]\n");
+  printf("wgrep: searchterm [file ...]\n");
   exit(1);
  }
+
+ char* const pattern = argv[1]; // search item
 
  char* line = NULL;
  size_t linecap = 0;
  ssize_t linelen; 
+ 
 
+ if (argc == 2) {
+  while((linelen = getline(&line, &linecap, stdin)) > 0) {
+   if (strstr(line, pattern)) {
+    fputs(line, stdout);
+   }
+  }
+  free(line);
+  exit(0);
+ }
+
+ 
  return 0;
 }
