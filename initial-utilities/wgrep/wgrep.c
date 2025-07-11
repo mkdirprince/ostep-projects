0a795cb8605e44ed22052e7dc8e68c2e076ad9ae
mkdirprince <mkdirprince@github.com>
2025-07-11 20:35:42 +0000

    define variables to be used for getline

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -4,9 +4,13 @@
 int main(int argc, char* argv[argc + 1]) {
 
  if (argc < 2) {
-  printf("wgrep: searchitem [file..]\n");
+  printf("wgrep: searchitem [file...]\n");
   exit(1);
  }
+
+ char* line = NULL;
+ size_t linecap = 0;
+ ssize_t linelen; 
 
  return 0;
 }
